8a21b4a49eb5a9b81151e0c38ff9af7caadaf9e3
ivanduva <duva.ivan@outlook.com>
2018-04-08 14:07:24 -0300

    Agrega documentación y migración

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from app import db
 
 class Product(db.Model):
+    """
+    Clase producto
+    attr id: la clave primaria del producto
+    attr name: una descripción del producto
+    attr price: precio unitario del producto
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), index=True)
     price = db.Column(db.Float, index=True)
@@ -10,7 +16,10 @@ class Product(db.Model):
 
     @property
     def serialize(self):
-        """Return object data in easily serializeable format"""
+        """
+        Transforma el objeto en un formato serializable
+        :return:
+        """
         return {
             'id': self.id,
             'name': self.name,
--- app/routes.py
+++ app/routes.py
@@ -8,11 +8,18 @@ def hello():
 
 @app.route("/product", methods=['GET', 'POST'])
 def products():
+    """
+    Endpoint para obtener todos los productos o crear uno nuevo
+    :return:
+    """
     if request.method == 'POST':
+        # Crea un nuevo producto recibiendo un JSON con atributos name y price
+        # Ejemplo: {'name': 'Tenedor', 'price': 50}
         p = Product(name=request.get_json()['name'], price=request.get_json()['price'])
         db.session.add(p)
         db.session.commit()
         return jsonify(p.serialize)
     else:
+        # Obtiene todos los productos disponibles
         p = Product.query.all()
         return jsonify([i.serialize for i in p])
