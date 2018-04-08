2947ce746f21ea698f9b84848311d7397338b77b
ivanduva <duva.ivan@outlook.com>
2018-04-08 13:15:27 -0300

    Agrega model de Producto y endpoint. Inicializa algunas instancias

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,20 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+from app import models
 migrate = Migrate(app, db)
 
 from app import routes
+
+from app.models import Product
+products = Product.query.all()
+
+if not products:
+    p = Product(name="Silla", price=500)
+    db.session.add(p)
+    p = Product(name="Mesa", price=2000)
+    db.session.add(p)
+    p = Product(name="Vaso", price=150)
+    db.session.add(p)
+    p = Product(name="Individual", price=250)
+    db.session.add(p)
